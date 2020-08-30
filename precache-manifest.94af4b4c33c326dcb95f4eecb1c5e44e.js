38d51736bc6875a91750fe9ecd8613dc261b074c
hellomaryjo <majorx3music@gmail.com>
2020-08-29 21:34:17 -0500

    Updates

---
+++ precache-manifest.94af4b4c33c326dcb95f4eecb1c5e44e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cb4c62e62d9b314c03659a4bdbfe90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72d148734bfa7a00c6cb",
+    "url": "./static/css/main.35f19aba.chunk.css"
+  },
+  {
+    "revision": "1a0de00281648717cf91",
+    "url": "./static/js/2.e38d3356.chunk.js"
+  },
+  {
+    "revision": "72d148734bfa7a00c6cb",
+    "url": "./static/js/main.a6dc2e1c.chunk.js"
+  },
+  {
+    "revision": "1bd913b0b13c806c95ec",
+    "url": "./static/js/runtime-main.8cc3b58f.js"
+  }
+]);
